56f57f3f23d5cbdad0671f94a7179656f1a56427
Luiz <clemente10claro@gmail.com>
2023-02-15 10:05:42 -0300

    Initial Commit

---
+++ BuscadorDeCEP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuscadorDeCEP
+//
+//  Created by Luiz Felipe Clemente Claro on 15/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
